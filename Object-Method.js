a1d89f14634b35e8916a35b92bd809859bb771f8
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2026-09-10 16:04:52 +0800

    Simple Object: Extract keys

---
+++ Object-Method.js
@@ -0,0 +1,7 @@
+// Object.keys()
+// Returns an array containing the keys (property names) of an object.
+
+// 1. Simple Object: Extract keys
+const user = { name: "Alice", age: 28, role: "Admin" };
+console.log(Object.keys(user));
+// Output: ['name', 'age', 'role']
