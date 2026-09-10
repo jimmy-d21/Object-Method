cb41f8cbc76bad204f489512482c841e6c324f40
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2026-09-10 16:05:51 +0800

    Array: Get array indices as string keys

--- Object-Method.js
+++ Object-Method.js
@@ -5,3 +5,8 @@
 const user = { name: "Alice", age: 28, role: "Admin" };
 console.log(Object.keys(user));
 // Output: ['name', 'age', 'role']
+
+// 2. Array: Get array indices as string keys
+const colors = ["red", "green", "blue"];
+console.log(Object.keys(colors));
+// Output: ['0', '1', '2']
